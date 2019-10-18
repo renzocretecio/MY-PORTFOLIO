453c85640f5954d8ea4f07388f4b787688c12943
unknown <renzocretecio@gmail.com>
2019-10-19 00:14:55 +0800

    added text animation

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -8,11 +8,28 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
 
+  private homeH1: string = "RENZO R. CRETECIO";
+  private displayTextH1: string = "";
+
+  typeWriter(that) {
+    let totalLengthH1 = that.homeH1.length;
+    let currentLengthH1 = that.displayTextH1.length;
+
+    if (currentLengthH1 < totalLengthH1) {
+      that.displayTextH1 += that.homeH1[currentLengthH1];
+    }
+    setTimeout(that.typeWriter, 200, that);
+  }
+
   img = ["../../assets/img/Logo2.jpg",
     "../../assets/img/Mev2.jpg"];
-  constructor() { }
+  constructor() {
+
+
+  }
 
   ngOnInit() {
+    this.typeWriter(this);
   }
 
   flip() {
