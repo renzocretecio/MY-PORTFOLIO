e02f7de0680960b26e0a3428965ea5021208cac2
unknown <renzocretecio@gmail.com>
2019-10-21 16:35:46 +0800

    page not found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { ResumeComponent } from './body/resume/resume.component';
 import { HomeComponent } from './body/home/home.component';
 import { ProjectsComponent } from './body/projects/projects.component';
+import { PageNotFoundComponent } from './body/page-not-found/page-not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 const routes: Routes = [
+  {path: '', component: NavigationComponent,
+children: [
   { path: '', component: HomeComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'resume', component: ResumeComponent },
-  { path: 'projects', component: ProjectsComponent }
+  { path: 'projects', component: ProjectsComponent },
+]},
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
